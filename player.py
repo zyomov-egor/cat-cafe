997fb03990df72f0c91c3fb2bbc96746417dc660
Георгий <zyomov.egor@yandex.ru>
2024-07-03 11:09:25 +0300

    11

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
-from scr.house import House
+from dice import Dice
+from house import House
+from playerinteractions import PlayerInteraction
+
+
 class Player():
-    def __init__(self, name, House, PlayerInteraction, Dice):
+    def __init__(self, name : str, house : House = None, playerinteraction: PlayerInteraction=None, dice : Dice=None):
         self.name = name
-        self.house = House """"(class?)"""
-        self.type = PlayerInteraction"""(class?)"""
-        self.choose_dice = Dice """(class?)"""
+        self.house = house
+        self.type = playerinteraction
+        self.choose_dice = dice
     def __repr__(self):
         pass
 
@@ -12,4 +16,10 @@ class Player():
         pass
 
     def choose_action(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
         pass
