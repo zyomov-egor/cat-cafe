002a1e9d50cb1f612609ea57d2b083bf838ba4f9
Георгий <zyomov.egor@yandex.ru>
2024-07-03 03:21:41 +0300

    10

--- dice.py
+++ dice.py
@@ -1,23 +1,26 @@
 import json
 from random import randint
-SIDES = [1, 2, 3, 4, 5, 6]
 class Dice():
-    def __init__(self):
-        self.current_side = SIDES[randint(0, 5)]
+    SIDES = [1, 2, 3, 4, 5, 6]
+    def __init__(self, side=None):
+        if side != None and 1 <= side <= 6:
+            self.current_side = side
+        else:
+            self.roll()
 
     def __repr__(self):
-        return str(self.side)
+        return str(self.current_side)
 
     def roll(self):
-        self.current_side = SIDES[randint(0, 5)]
+        self.current_side = self.SIDES[randint(0, 5)]
 
     def save(self):
-        with open('data1.json', 'w') as file:
-            json.dump({"current_side":self.current_side}, file)
-            return {"current_side":self.current_side}
+        # with open('data1.json', 'w') as file:
+        #     json.dump({"current_side":self.current_side}, file)
+        return repr(self)
 
-    @staticmethod
-    def load():
-        with open('data1.json', 'r') as file:
-            s = json.load(file)
-        return s["current_side"]
+    @classmethod
+    def load(cls, value: str):
+    #     with open('data1.json', 'r') as file:
+    #         s = json.load(file)
+        return cls(value)
--- test_dice.py
+++ test_dice.py
@@ -2,24 +2,25 @@ import json
 
 from dice import Dice
 
-
 def test_init():
     c = Dice()
     assert c.current_side in [1, 2, 3, 4, 5, 6]
+    a = Dice(5)
+    assert a.current_side == 5
 
 def test_repr():
-    c = Dice()
+    c = Dice(3)
     assert type(c) == Dice
 
 def test_save():
-    c = Dice()
+    c = Dice(5)
     c.save()
-    with open('data1.json', 'r') as file:
-        a = json.load(file)
-    assert a == {"current_side":c.current_side}
-    assert type(a) == dict
+    # with open('data1.json', 'r') as file:
+    #     a = json.load(file)
+    # assert a == {"current_side":c.current_side}
+    # assert type(a) == dict
 
 def test_load():
-    c = Dice()
-    c.save()
-    assert c.load() == c.current_side
+    c = Dice(2)
+    c = c.load(1)
+    assert c.current_side == 1
