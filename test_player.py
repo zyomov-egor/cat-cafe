da36bfa67985d6bd7a952ef377d0a1d226ed9969
Георгий <zyomov.egor@yandex.ru>
2024-07-04 18:21:24 +0300

    test_Player

--- test_player.py
+++ test_player.py
@@ -4,30 +4,72 @@ from player import Player
 
 
 def test_init():
-    p = Player(name='George', house=House(), dice=Dice())
+    p = Player(name='George', house=House())
     assert p.name == 'George'
     assert p.tower_lst.house == [["NAN", "NAN", "", "NAN", "NAN"],
-                          ["NAN", "", "NAN", "", "NAN"],
-                          ["", "NAN", "", "NAN", "NAN"],
-                          ["NAN", "", "NAN", "", "NAN"],
-                          ["NAN", "NAN", "", "NAN", "NAN"],
-                          ["NAN", "", "NAN", "", "NAN"],
-                          ["", "NAN", "NAN", "NAN", ""],
-                          ["NAN", "", "NAN", "", "NAN"],
-                          ["", "NAN", "", "NAN", ""],
-                          ["NAN", "", "NAN", "", "NAN"],
-                          ["", "NAN", "", "NAN", ""],
-                          ["NAN", "", "NAN", "", "NAN"]]
-    assert p.choosen_dice.current_side in [1, 2, 3, 4, 5, 6]
+                                  ["NAN", "", "NAN", "", "NAN"],
+                                  ["", "NAN", "", "NAN", "NAN"],
+                                  ["NAN", "", "NAN", "", "NAN"],
+                                  ["NAN", "NAN", "", "NAN", "NAN"],
+                                  ["NAN", "", "NAN", "", "NAN"],
+                                  ["", "NAN", "NAN", "NAN", ""],
+                                  ["NAN", "", "NAN", "", "NAN"],
+                                  ["", "NAN", "", "NAN", ""],
+                                  ["NAN", "", "NAN", "", "NAN"],
+                                  ["", "NAN", "", "NAN", ""],
+                                  ["NAN", "", "NAN", "", "NAN"]]
 def test_repr():
-    c = Player(name='George', house=House())
+    a =  [["NAN", "NAN", 1, "NAN", "NAN"],
+          ["NAN", "", "NAN", "", "NAN"],
+          ["", "NAN", "", "NAN", "NAN"],
+          ["NAN", "", "NAN", "", "NAN"],
+          ["NAN", "NAN", "", "NAN", "NAN"],
+          ["NAN", "", "NAN", 5, "NAN"],
+          ["", "NAN", "NAN", "NAN", ""],
+          ["NAN", "", "NAN", "", "NAN"],
+          ["", "NAN", "", "NAN", ""],
+          ["NAN", "", "NAN", "", "NAN"],
+          ["", "NAN", "", "NAN", ""],
+          ["NAN", 2, "NAN", "", "NAN"]]
+    c = Player(name='George', house=House(a))
     print(c)
 
-def test_choose_dice():
-    pass
+def test_choose_dice_ai():
+    a = [["NAN", "NAN", 1, "NAN", "NAN"],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", "NAN"],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["NAN", "NAN", "", "NAN", "NAN"],
+         ["NAN", "", "NAN", 5, "NAN"],
+         ["", "NAN", "NAN", "NAN", ""],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", ""],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", ""],
+         ["NAN", 2, "NAN", "", "NAN"]]
+    c = Player('Dimon', House(a))
+    assert type(c.choose_dice([2, 4, 1])) == int
+    assert c.choose_dice([2, 4, 1]) in [2, 4, 1]
 
-def test_choose_action():
-    pass
+def test_choose_action_ai():
+    a = [["NAN", "NAN", 1, "NAN", "NAN"],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", "NAN"],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["NAN", "NAN", "", "NAN", "NAN"],
+         ["NAN", "", "NAN", 5, "NAN"],
+         ["", "NAN", "NAN", "NAN", ""],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", ""],
+         ["NAN", "", "NAN", "", "NAN"],
+         ["", "NAN", "", "NAN", ""],
+         ["NAN", 2, "NAN", "", "NAN"]]
+    c = Player('Dimon', House(a))
+    c.choose_action([2, 6])
+    assert type(c.choose_action([2, 6])) == tuple
+    assert c.choose_action([2, 6])[0] in [1, 2, 3, 4, 5]
+    assert c.choose_action([2, 6])[1] in [1, 2, 3, 4, 5, 6]
+    assert c.choose_action([2, 6])[2] in [1, 2, 3, 4, 5, 6]
 
 def test_save():
     pass
