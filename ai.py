922e71c75f74728e9af22f4dca4b50aefb251190
Георгий <zyomov.egor@yandex.ru>
2024-07-04 16:51:57 +0300

    Human and AI

--- ai.py
+++ ai.py
@@ -1,28 +1,31 @@
 from random import randint
 from random import choice
-
 from house_new import House
 
 items = [1, 2, 3, 4, 5, 6]
 class AI():
-    def choose_dice(self, lst_dices: list=None):
+    def choose_dice(self, dice_list: list=None):
         """выбирает произвоьный кубик из трех или двух в зависимости от хода, i_player = True если игрок выбирает кубик первым"""
-        return choice(lst_dices)
+        return choice(dice_list)
 
-    def choose_action(self, house: House=None, dices=(int, int)):
+    def choose_action(self, house: House=None, dices=[int, int]):
         tower = randint(1,5)
-        if tower % 2 == 1:
-            if (house[12 - 2 * dices[0]][dices[1] - 1] != "NAN") and (house[12 - 2 * dices[0]][dices[1] - 1] not in items):
-                return (tower, dices[0], dices[1])
-            elif (house[12 - 2 * dices[1]][dices[0] - 1] != "NAN") and (house[12 - 2 * dices[1]][dices[0] - 1] not in items):
-                return (tower, dices[1], dices[0])
-            else:
-                return None
+        if house._get(tower, dices[0]) == "":
+            return tower, dices[0], dices[1]
+        elif house._get(tower, dices[1]) == "":
+            return tower, dices[1], dices[0]
         else:
-            if (house[13 - 2 * dices[0]][dices[1] - 1] != "NAN") and (house[13 - 2 * dices[0]][dices[1] - 1] not in items):
-                return (tower, dices[0], dices[1])
-            elif (house[13 - 2 * dices[1]][dices[0] - 1] != "NAN") and (house[13 - 2 * dices[1]][dices[0] - 1] not in items):
-                return (tower, dices[1], dices[0])
-            else:
-                return None
+            return None
+
+# class Answer():
+#     def __init__(self, tower, floor, item):
+#         self.tower = tower
+#         self.floor = floor
+#         self.item = item
+#     def __repr__(self):
+#         return f'{self.tower}, {self.floor}, {self.item}'
+#
+#     def f(self):
+#         return self.tower, self.floor, self.item
+
 
--- human.py
+++ human.py
@@ -1,9 +1,11 @@
 from dice import Dice
+from house_new import House
 
 
 class Human():
     def choose_dice(self, dice_list: list[int]):
         while True:
+            print(dice_list)
             res = input('Выберите 1 кубик: ')
             dice = Dice.load(res)
             if dice in dice_list:
@@ -11,7 +13,30 @@ class Human():
                 return dice
             else:
                 print('Такого кубика нет!')
-    def choose_action(self):
+    def choose_action(self, house: House=None, dices=[int, int]):
+        while True:
+            while True:
+                tower = int(input('выберите номер башни башни: '))
+                if not(1 <= tower <= 5):
+                    print('Такой башни нет!')
+                    continue
+                break
+            while True:
+                floor = int(input('выберите какой кубик отвечает за этаж башни: '))
+                if floor not in dices:
+                    print('Такого кубика нет!')
+                    continue
+                break
+            item = dices.remove(floor)[0]
+            if house._get(tower, dices[0]) == "":
+                return tower, floor, item
+            else:
+                ans = input('Сделайте другой выбор или пропустите ход \nВы хотите пропустить ход? ')
+                if ans in ['да', 'Да', 'ДА', 'y', 'Y', 'Yes', 'YEs', 'YES']:
+                    return None
+                else:
+                    continue
+
 
 
 
